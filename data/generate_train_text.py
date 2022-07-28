9e45b81ed20832b3017f5f89afb727665dc658ff
Eko Rudiawan Jamzuri <eko.rudiawan@gmail.com>
2022-07-28 14:10:36 +0700

    add source to generate train.txt

---
+++ data/generate_train_text.py
@@ -0,0 +1,22 @@
+import os
+import glob
+
+def main():
+    print("Start Generate train.txt")
+    dir_path = "/home/barelangfc/research/DRBox-V2/data/test/*.jpg"
+    with open('test.txt', 'w') as f:
+        for filename in glob.glob(dir_path):
+            #new_filename = filename.lower().replace('(', '_').replace(')','').replace(' ','')
+            #os.rename(filename, new_filename)
+            label_filename = filename.replace("jpg","jpg.rbox")
+            if(os.path.exists(label_filename)):
+                print(filename)
+                f.write(filename + " " + label_filename)
+                f.write('\r')
+                f.write('\n')
+            else:
+                print("Label file missing")
+                os.remove(filename)
+
+if __name__ == "__main__":
+    main()
