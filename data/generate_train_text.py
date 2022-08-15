bfbc24afb5db3fa4c1b1cb4d35d3fde61a2e7037
Eko Rudiawan Jamzuri <eko.rudiawan@gmail.com>
2022-08-15 20:55:37 +0700

    change extension

--- data/generate_train_text.py
+++ data/generate_train_text.py
@@ -3,12 +3,12 @@ import glob
 
 def main():
     print("Start Generate train.txt")
-    dir_path = "/home/barelangfc/research/DRBox-V2/data/test/*.jpg"
+    dir_path = "/home/ekorudiawan/research/DRBox-V2/data/test/*.jpg"
     with open('test.txt', 'w') as f:
         for filename in glob.glob(dir_path):
             #new_filename = filename.lower().replace('(', '_').replace(')','').replace(' ','')
             #os.rename(filename, new_filename)
-            label_filename = filename.replace("jpg","jpg.rbox")
+            label_filename = filename.replace("jpg","rbox")
             if(os.path.exists(label_filename)):
                 print(filename)
                 f.write(filename + " " + label_filename)
